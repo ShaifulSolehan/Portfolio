ea74af16c97453d2068626eea636fa74c8de14f6
ShaifulSolehan <shaifulsolehan@gmail.com>
2024-10-22 18:18:09 +0800

    Commit changes adding new project page and some styling

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,abort
 
 app = Flask(__name__)
 projects = [
@@ -25,6 +25,9 @@ projects = [
         "slug": "",
     },
 ]
+
+slug_to_project = {project["slug"]: project for project in projects}
+
 @app.route("/")
 def home():
     return render_template ("home.html",projects=projects)
@@ -36,3 +39,9 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
